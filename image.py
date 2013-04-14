db0d6c6c078f39ab0917c0d57fa3a5b7a04c222f
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-14 19:22:45 -0400

    worked on pixel and image classes

--- image.py
+++ image.py
@@ -1,7 +1,47 @@
 import Image
 
-im = Image.open ("castle.jpg")
 
-print im.format, im.size, im.mode
+#representation of image for seam carving
+class sc_Image
+    def __init__(self, pos, rgb): 
+    	self.width = pos[w]
+    	self.height = rgb
+    	self.pixels = pixels
 
-print list(im.getdata())
+
+class Pixel:
+    def __init__(self, pos, rgb): 
+    	self.pos = pos
+    	self.rgb = rgb
+    	self.energy = 0
+
+    # get the neighbors of a pixel given an image
+    def get_neighbors (self, image):
+    	raise NotImplementedError
+
+
+    def get_energy (self, algorithm, image) :
+    	if algorithm = 'e1':
+    		raise NotImplementedError
+
+    	elif algorithm = 'e2':
+    		raise NotImplementedError
+
+    	else
+    		raise Exception("%s is one of the implemented algorithms" %s algorithm)
+
+    # to string function
+    def __str__(self):
+        return "[%s - %s]" % (str(self.pos), str(self.rgb))
+
+def data_from_image(filepath):
+	image = {}
+	im = Image.open (filepath)
+	width, height = im.size
+	for h in range(height):
+		for w in range(width):
+			image[(w,h)] = Pixel( (w,h), im.getpixel((w,h)) )
+	return image
+
+def image_from_data (image):
+	raise NotImplementedError
---
+++ main.py
@@ -0,0 +1,7 @@
+import image
+
+image = data_from_image("castle.jpg")
+
+#get the image at coordinate (0,0)
+print image[(0,0)]
+
