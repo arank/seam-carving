cc70b86f20333f785b4695db5fb97be1dca9c18c
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-15 00:01:06 -0400

    edited main.py example code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import image
 
 image = data_from_image("castle.jpg")
 
-image.enlarge( 'horizontal', 300 , 'e1', 'dyn')
+image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
 image.to_jpeg("new_castle.jpg")
 
