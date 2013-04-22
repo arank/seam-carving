78717a7ef805e2caba48bbb87a1634ae05ce8f4f
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-04-21 21:30:25 -0400

    bah

--- energy.py
+++ energy.py
@@ -2,7 +2,7 @@
 #gets energy using e1 algorithm. helper methods may be added later
 def e1(pixel, neighbors):
         x, y = pixel.pos
-        [n1, n2, n3, n4, n5, n6, 7n, n8] = neighbors
+        [n1, n2, n3, n4, px, n5, n6, n7, n8] = neighbors
         """
         | n1 n2 n3 |
         | n4 px n5 |
@@ -10,7 +10,7 @@ def e1(pixel, neighbors):
         """
         pos_dx = 4 * pixel.rgb - 2 * n4.rgb - n1.rgb - n6.rgb
         neg_dx = 4 * pixel.rgb - 2 * n5.rgb - n3.rgb - n8.rgb
-        dx = ps_dx - neg_dx
+        dx = pos_dx - neg_dx
 
         pos_dy = 4 * pixel.rgb - 2 * n7.rgb - n8.rgb - n6.rgb
         neg_dy = 4 * pixel.rgb - 2 * n2.rgb - n3.rgb - n1.rgb
