ee72fed10b3dbfd93c4686672f7e572b60b72c23
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-21 21:31:51 -0400

    added test_energy.py

---
+++ test_energy.py
@@ -0,0 +1,10 @@
+import image
+
+im = image.sc_Image.from_filepath2("castle.jpg")
+
+print im.get_next_seam("dijk")
+
+# image.shrink( 'horizontal', 300 , 'e1', 'dyn')
+
+im.to_jpeg("new_castle.jpg")
+
