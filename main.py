9ce2da70ecf9ca6429d703ef7fe568a31fe8742b
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-25 21:49:24 -0400

    changed main.py back to landscape

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import image
 
-im = image.sc_Image.from_filepath2("skateboarder.jpg")
+im = image.sc_Image.from_filepath2("landscape.jpg")
 
 im.shrink(50)
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("new_skateboarder.jpg")
+im.to_jpeg("new_landscape2cached.jpg")
 
