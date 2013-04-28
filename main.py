33360bdee0977b2b80c8d91d929d28bdceccb18d
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-04-28 16:01:18 -0400

    more merge conflicts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import image
 
-im = image.sc_Image.from_filepath2("skateboarder.jpg")
+im = image.sc_Image.from_filepath2("dolphin.jpg")
 
 im.to_energy_pic('skateboarder_energy2.jpg')
 im.shrink(50, alg = 'dyn', orientation = 'horizontal')
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("skateboarder_horz.jpg")
+im.to_jpeg("dolphin2.jpg")
+
 
