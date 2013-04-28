198e59bc0ec672cc728377e6ddf9587fdb581e59
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-28 14:38:48 -0400

    fixed to_energy pic to work after caching was implemented

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ im = image.sc_Image.from_filepath2("dolphin.jpg")
 
 im.to_energy_pic('skateboarder_energy2.jpg')
 im.shrink(50, alg = 'dyn', orientation = 'horizontal')
+im.to_energy_pic('dolphin_energy2.jpg')
+im.shrink(50, alg = 'dyn')
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("dolphin2.jpg")
+im.to_jpeg("dolphin_kroon.jpg")
 
 
