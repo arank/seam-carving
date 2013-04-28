f500b98523c1e7643eb63d44ae839d7da34a4007
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-28 13:56:48 -0400

    changed main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import image
 
-im = image.sc_Image.from_filepath2("castle.jpg")
+im = image.sc_Image.from_filepath2("skateboarder.jpg")
 
 im.shrink(100, alg = 'dyn')
 # image.shrink( 'horizontal', 300 , 'e1', 'dyn')
 
-im.to_jpeg("new_castle2.jpg")
+im.to_jpeg("new_skateboarder2.jpg")
 
