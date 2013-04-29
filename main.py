3a554069933796fbb7a005c4870f56f023148df7
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-04-28 21:38:03 -0400

    added skateboarder enlargement to main

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ im.enlarge(80, alg = 'dyn', energy = "e1", orientation = 'horizontal')
 
 im.to_jpeg("dolphin_horizontal_enlarge.jpg")
 
+
+
+
+#enlarging skateboarder by 80 pixels
+
+im = image.sc_Image.from_filepath2("skateboarder.jpg")
+im.enlarge(80,orientation = 'horizontal', energy = 'e1', alg = 'dyn')
+im.to_jpeg("skateboarder_enlarged.jpg")
